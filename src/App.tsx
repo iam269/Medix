9e64195274775601bed28f2d7a0ebe73897d2c36
Aurel Ionita <ionitaaurel32@gmail.com>
2026-01-18 21:13:20 +0200

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import ServiceDetail from "./pages/ServiceDetail";
+import Team from "./pages/Team";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +15,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <Index />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/services/:serviceId" element={<ServiceDetail />} />
+          <Route path="/team" element={<Team />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
